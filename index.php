c84e708ba3639bb176edc52e694ad8e219ac392a
Matt Stolarz <mstolarz@besentient.com>
2016-09-14 14:37:31 -0400

    Adding atomic structure, index and header

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=yes">
+	<title>Blank Creative</title>
+    <?php require_once("assets/php/head.php"); ?>
+</head>
+<body>
+	<?php require_once("assets/php/header.php"); ?>
+	
+
+</body>
+</html>
+
+</body>
+</html>
